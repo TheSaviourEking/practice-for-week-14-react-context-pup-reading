57ddeb36f7eb41af3a9dd4bd1cdcc14a089c805e
TheSaviourEking <saviour.eking@outlook.com>
2024-05-26 16:01:43 +0100

    chore: create the PupContext

--- src/context/PupContext.js
+++ src/context/PupContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useContext } from 'react'
 import speedy from '../pups/speedy-pup.jpg';
+// import banana from '../pups/banana-pup.jpg';
 
 export const PupContext = createContext();
 
